7ee422ad3aaeba4eed3aec9d73788df087629b1f
acse-08dbaa34 <jiajia.zheng22@imperial.ac.uk>
2022-10-09 11:04:08 +0100

    Add create_dataframe.py

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -2,8 +2,9 @@ import numpy as np
 from scipy.ndimage import gaussian_filter
 from scipy import misc
 
+import pandas as pd
 
-__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'data_frame']
 
 
 def rand_array(shape):
@@ -14,3 +15,9 @@ def smooth_image(a, sigma=1):
 
 def my_mat_solve(A, b):
     return A.inv()*b
+
+def data_frame(list1, list2):
+    s1 = pd.Series(list1)
+    s2 = pd.Series(list2)
+    data_dict = {"name":s1, "number":s2}
+    return pd.DataFrame(data_dict)
---
+++ scripts/create_dataframe.py
@@ -0,0 +1,6 @@
+from envtest import data_frame
+
+list1 = ['cat', 'dog', 'monkey']
+list2 = [4,5,6]
+df = data_frame(list1, list2)
+print(df)
